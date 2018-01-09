dc684c3f99ece50c60cd89aae2fa2440797243ca
youKnow44 <yasscomoko@gmail.com>
2018-01-09 17:15:00 +0100

    Configuration

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/public/codeIgniter/';
+//$config['base_url'] = 'http://localhost/public/codeIgniter/';
+$config['base_url'] = 'http://localhost/login/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +36,7 @@ $config['base_url'] = 'http://localhost/public/codeIgniter/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
